faee11533a8b00846313cd4ef98eb1708ed6bb0a
Vladislav Aleinik <valeinik@ispras.ru>
2023-09-09 21:35:56 +0300

    examples: solve task №02 from contest №0
    Introduce the concept of program contract.
    Print error message for all non-contract inputs.

---
+++ examples/02_program_contracts/adder.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+    short num1, num2 = 0;
+
+    // Parse program input:
+    int num_inputs = scanf("%hd\n%hd", &num1, &num2);
+    if (num_inputs < 2)
+    {
+        printf("adder: expected input \"<addentum1> <addentum2>\\n\"\n");
+        return 1;
+    }
+
+    // Cast numbers to long and check if they overflow on addition:
+    int num1_ext = num1;
+    int num2_ext = num2;
+    int sum_ext = num1_ext + num2_ext;
+    if (sum_ext < SHRT_MIN || sum_ext > SHRT_MAX)
+    {
+        printf("adder: %hd+%hd is too big to be represented in short", num1, num2);
+        return 1;
+    }
+
+    // Perform short integer addition:
+    // NOTE: if num1 + num2 is not representable by short, a problem might arise.
+    short sum = num1 + num2;
+
+    // Print result:
+    printf("%hd\n", sum);
+
+    return 0;
+}
